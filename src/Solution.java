f81892fe158f23bcb4d95f2aa077bb3de157765f
Sophia Xiang <xxy99410@gmail.com>
2026-02-09 22:55:30 +0800

    56. 合并区间

---
+++ src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) {
+            return new int[0][2];
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(interval -> interval[0]));
+        List<int[]> merged = new ArrayList<>();
+        for (int[] interval : intervals) {
+            int L = interval[0], R = interval[1];
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < L) {
+                merged.add(new int[]{L, R});
+            } else {
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] intervals = {
+                {1, 3},
+                {2, 6},
+                {8, 10},
+                {15, 18}
+        };
+        int[][] result = solution.merge(intervals);
+        System.out.println(Arrays.deepToString(result));
+    }
+}
