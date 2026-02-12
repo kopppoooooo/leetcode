05298cb306e3a07278ad38373cdfbffd2f3d5a57
Sophia Xiang <xxy99410@gmail.com>
2026-02-12 23:05:02 +0800

    238. 除了自身以外数组的乘积

--- src/Solution.java
+++ src/Solution.java
@@ -1,37 +1,33 @@
 import java.util.Arrays;
 
 class Solution {
-    public void rotate(int[] nums, int k) {
-//        int n = nums.length;
-//        int[] newArr = new int[n];
-//        for (int i = 0; i < n; ++i) {
-//            newArr[(i + k) % n] = nums[i];
-//        }
-//        System.arraycopy(newArr, 0, nums, 0, n);
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
 
-        // 法2: 三次反转 O(n)/O(1)
-        int n = nums.length;
-        k = k % n;
-        reverse(nums, 0, n - 1);  // 整体反转
-        reverse(nums, 0, k - 1);  // 反转前k个
-        reverse(nums, k, n - 1);  // 反转剩余部分
-    }
-
-    private void reverse(int[] nums, int start, int end) {
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+        // answer[i] 表示索引 i 左侧所有元素的乘积
+        // 因为索引为 '0' 的元素左侧没有元素， 所以 answer[0] = 1
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
         }
+
+        // R 为右侧所有元素的乘积
+        // 刚开始右边没有元素，所以 R = 1
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            // 对于索引 i，左边的乘积为 answer[i]，右边的乘积为 R
+            answer[i] = answer[i] * R;
+            // R 需要包含右边所有的乘积，所以计算下一个结果时需要将当前值乘到 R 上
+            R *= nums[i];
+        }
+        return answer;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        int k = 3;
-        solution.rotate(nums, k);
-        System.out.println(Arrays.toString(nums));
+        int[] nums = {1, 2, 3, 4};
+        int[] result = solution.productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
     }
 }
