70633a6c706d995304420a113d91e9f86fb08866
Sophia Xiang <xxy99410@gmail.com>
2026-02-15 23:01:20 +0800

    73. 矩阵置零

--- src/Solution.java
+++ src/Solution.java
@@ -1,30 +1,39 @@
+import java.util.Arrays;
+
 class Solution {
-    public int firstMissingPositive(int[] nums) {
-        // 原地哈希，数组做哈希
-        int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] <= 0) {
-                nums[i] = n + 1;
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
             }
         }
-        for (int i = 0; i < n; ++i) {
-            int num = Math.abs(nums[i]);
-            if (num <= n) {
-                nums[num - 1] = -Math.abs(nums[num - 1]);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
             }
         }
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] > 0) {
-                return i + 1;
-            }
-        }
-        return n + 1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {3, 4, -1, 1};
-        int result = solution.firstMissingPositive(nums);
-        System.out.println(result);
+
+        int[][] matrix = {
+                {0, 1, 2, 0},
+                {3, 4, 5, 2},
+                {1, 3, 1, 5}
+        };
+
+        solution.setZeroes(matrix);
+
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
