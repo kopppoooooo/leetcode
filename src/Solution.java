4469e483b2bdc3a30dd5234fe7352ab37758f13c
Sophia Xiang <xxy99410@gmail.com>
2026-02-17 10:12:37 +0800

    240. 搜索二维矩阵 II

--- src/Solution.java
+++ src/Solution.java
@@ -1,51 +1,63 @@
-import java.util.Arrays;
-
 class Solution {
-    public void rotate(int[][] matrix) {
-        // 方法二：不使用额外内存空间的旋转 -> 原地旋转
-//        int n = matrix.length;
-//        for (int i = 0; i < n / 2; ++i) {
-//            for (int j = 0; j < (n + 1) / 2; ++j) {
-//                int temp = matrix[i][j];
-//                matrix[i][j] = matrix[n - j - 1][i];
-//                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
-//                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
-//                matrix[j][n - i - 1] = temp;
+    public boolean searchMatrix(int[][] matrix, int target) {
+//        // 方法一：暴力 O(mn) O(1)
+//        for (int[] row : matrix) {
+//            for (int element : row) {
+//                if (element == target) {
+//                    return true;
+//                }
 //            }
 //        }
-        // 方法三：用翻转代替旋转
-        int n = matrix.length;
-        // 水平翻转
-        for (int i = 0; i < n / 2; ++i) {
-            for (int j = 0; j < n; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[n - i - 1][j];
-                matrix[n - i - 1][j] = temp;
+//        return false;
+
+//        // 方法二：按行二分查找 O(mlogn) O(1)
+//        for (int[] row : matrix) {
+//            int index = search(row, target);
+//            if (index >= 0) {
+//                return true;
+//            }
+//        }
+//        return false;
+
+        // 方法三：Z字形查找（右上角往左、下查找） O(m+n) O(1)
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                --y;
+            } else {
+                ++x;
             }
         }
-        // 主对角线翻转
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        return false;
+    }
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
+        return -1;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
 
-        int[][] matrix = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
-        };
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 
-        solution.rotate(matrix);
+        boolean result = solution.searchMatrix(matrix, 5);
 
-        for (int[] row : matrix) {
-            System.out.println(Arrays.toString(row));
-        }
+        System.out.println(result);
     }
 }
